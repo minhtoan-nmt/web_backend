aa5b1da34157e4b81579861e030314852228674f
hcmutmtk22 <nguyenminhtoan204@gmail.com>
2025-05-09 11:14:38 +0700

    #5 - delete unnecessary files

--- helpers/response.php
+++
@@ -1,9 +0,0 @@
-function ok($data) {
-  echo json_encode(['success'=>true, 'data'=>$data]);
-  exit;
-}
-function err($msg, $code=400) {
-  http_response_code($code);
-  echo json_encode(['success'=>false, 'error'=>$msg]);
-  exit;
-}
