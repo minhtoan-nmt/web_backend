ad4368b441c48d71a7df37da3909c8597e3645ce
hcmutmtk22 <nguyenminhtoan204@gmail.com>
2025-05-09 10:12:52 +0700

    Merge all endpoints

--- api/user/login.php
+++ api/user/login.php
@@ -9,17 +9,24 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     exit;
 }
 
+$json = file_get_contents('php://input');
+
+// Decode the JSON to an associative array
+$data = json_decode($json, true);
+
+// var_dump($data);
+
 $query = mysqli_query($conn, "SELECT * 
 FROM `accounts` 
-WHERE `username` = '" . $_POST["username"] . "' AND `password` = '". $_POST["password"] . "'");
+WHERE `username` = '" . $data["username"] . "' AND `password` = '". $data["password"] . "'");
 
 $row = mysqli_fetch_assoc($query);
 if (!$row) {
-    echo "Thông tin đăng nhập không chính xác";
+    echo json_encode(false);
     exit;
 }
 
 // header("Set-Cookie: token=" . $row["cart_id"] . "; Expires=" . time()+60*60*24);
-echo $row;
+echo json_encode($row["cart_id"]);
 
 ?>
--- faq_about/config/database.php
+++ faq_about/config/database.php
@@ -1,6 +1,6 @@
 <?php
 $host = 'localhost';
-$dbname = 'faq_about';
+$dbname = 'ecommerce';
 $username = 'root';
 $password = '';
 
